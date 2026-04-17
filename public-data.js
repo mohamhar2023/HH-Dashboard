260f50ae24860a29045d6aaef33518ad03bc445b
Harry Mohammed J.R <harry.mohammed.jr@gmail.com>
2026-04-17 00:57:59 -0400

    Use sanitized dashboard snapshot export

---
+++ public-data.js
@@ -0,0 +1,15 @@
+window.HH_DASHBOARD_DATA = {
+  "generatedAt": "2026-04-17T04:48:18Z",
+  "source": "sanitized-export",
+  "projects": [
+    {
+      "projectId": "AMA-001",
+      "projectName": "Amas House",
+      "income": 2500.0,
+      "expenses": 1823.7,
+      "materials": 1586.7,
+      "profit": -910.4,
+      "status": "Active"
+    }
+  ]
+};
